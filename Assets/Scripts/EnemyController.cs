990e0a36ee6ea88d040a75ae838bdc2ed82ea219
yy-yusuke0122 <61263724+yy-yusuke0122@users.noreply.github.com>
2020-06-15 02:38:46 +0900

    Enemy出現のスクリプトを作成

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    float MoveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(-MoveSpeed * Time.deltaTime, 0.0f, 0.0f));
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // 敵のオブジェクト
+    [SerializeField]
+    GameObject enemy;
+
+    // スポーン間隔
+    [SerializeField]
+    float Interval;
+
+    // 経過時間
+    float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IncrementCool();
+        SpawnEnemy();
+    }
+
+    void IncrementCool()
+    {
+        time += Time.deltaTime;
+    }
+
+    void SpawnEnemy()
+    {
+        if (time > Interval)
+        {
+            Instantiate(enemy, this.transform);
+            time = 0.0f;
+        }
+    }
+}
