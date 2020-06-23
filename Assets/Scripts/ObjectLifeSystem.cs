cf125f986eb1930b538c05b22249372a0c139b73
3CPLUTO <ware364tky@gmail.com>
2020-06-23 12:28:28 +0900

    拠点のライフシステムにエネルギーをつけた。 回復値の設定はまだできない。

--- Assets/Scripts/ObjectLifeSystem.cs
+++ Assets/Scripts/ObjectLifeSystem.cs
@@ -6,17 +6,21 @@ public class ObjectLifeSystem : MonoBehaviour
 {
     [SerializeField]
     float HP = 100f;
-
+    float Enegy = 1000f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Enegy++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Enegy > 1000)
+        {
+            Enegy = 1000;
+            Debug.Log("満タンだよ");
+        }
     }
 
     public void Damage(float _val)
