8c70fca4f32a76aa7d307c01a3baefa8146fb5d0
3CPLUTO <ware364tky@gmail.com>
2020-06-23 12:44:39 +0900

    エディター上でいじれるようにした。

--- Assets/Scripts/ObjectLifeSystem.cs
+++ Assets/Scripts/ObjectLifeSystem.cs
@@ -6,16 +6,24 @@ public class ObjectLifeSystem : MonoBehaviour
 {
     [SerializeField]
     float HP = 100f;
+    [SerializeField]
     float Enegy = 1000f;
+    [SerializeField]
+    float Charg = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
-        Enegy++;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Enegy < 1000)
+        {
+            Enegy+=Charg;
+            //Debug.Log("回復中です");
+        }
         if (Enegy > 1000)
         {
             Enegy = 1000;
