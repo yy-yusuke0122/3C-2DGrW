5cf15256d3d5666cc4a1610918ec56ab99a8c250
3CPLUTO <ware364tky@gmail.com>
2020-06-24 15:19:06 +0900

    減算はまだできてない

---
+++ Assets/Scripts/BaseEnergy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnergy : MonoBehaviour
+{
+    [SerializeField]
+    float energy = 1000f;
+    [SerializeField]
+    float Charg = 0.1f;
+    float cut = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Energy comp = GetComponent<Energy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (energy < 1000)
+        {
+            energy += Charg;
+            //Debug.Log("回復中です");
+        }
+        if (energy > 1000)
+        {
+            energy = 1000;
+            Debug.Log("満タンだよ");
+        }
+    }
+}
--- Assets/Scripts/ObjectLifeSystem.cs
+++ Assets/Scripts/ObjectLifeSystem.cs
@@ -6,29 +6,18 @@ public class ObjectLifeSystem : MonoBehaviour
 {
     [SerializeField]
     float HP = 100f;
-    [SerializeField]
-    float Enegy = 1000f;
-    [SerializeField]
-    float Charg = 0.1f;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Enegy < 1000)
-        {
-            Enegy+=Charg;
-            //Debug.Log("回復中です");
-        }
-        if (Enegy > 1000)
-        {
-            Enegy = 1000;
-            Debug.Log("満タンだよ");
-        }
+
+       
     }
 
     public void Damage(float _val)
@@ -44,4 +33,5 @@ public class ObjectLifeSystem : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+    
 }
