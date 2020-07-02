f599bbae12d07a3200848c4c6465918ac6dfcb2c
3CPLUTO <ware364tky@gmail.com>
2020-07-02 14:15:40 +0900

    タイトルからゲーム終了可能

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Title : MonoBehaviour
 {
@@ -16,7 +19,14 @@ public class Title : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            SceneManager.LoadScene("TestMain");
+            SceneManager.LoadScene("Main");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
         }
     }
 }
